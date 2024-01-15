4a67ce534da916d390b48d18e37045982bea5476
Egor Santalov <egor.santaloff@yandex.ru>
2024-01-15 10:12:11 +0300

    complete day5(edit 1) tasks

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,43 @@
+package day5;
+
+public class Car {
+
+    private String model;
+
+    public void setModel(String userModel) {
+
+        model = userModel;
+    }
+
+    public String getModel() {
+
+        return model;
+    }
+
+    private String color;
+
+    public void setColor(String userColor) {
+
+        color = userColor;
+    }
+
+    public String getColor() {
+
+        return color;
+    }
+
+    private int year;
+
+    public void setYear(int userYear) {
+
+        year = userYear;
+    }
+
+    public int getYear() {
+
+        return year;
+    }
+
+
+}
+
--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -6,42 +6,7 @@ public class Task1 {
         myCar.setModel("Лада Седан");
         myCar.setColor("Баклажан");
         myCar.setYear(2013);
-        System.out.println("Модель: " + myCar.getModel());
-        System.out.println("Цвет: " + myCar.getColor());
-        System.out.println("Год: " + myCar.getYear());
-    }
+        System.out.println("Модель: " + myCar.getModel()+"\nЦвет: "+ myCar.getColor()+"\nГод: " + myCar.getYear());
+            }
 }
 
-class Car {
-    private String model;
-
-    public void setModel(String userModel) {
-        model = userModel;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    private String color;
-
-    public void setColor(String userColor) {
-        color = userColor;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    private int year;
-
-    public void setYear(int userYear) {
-        year = userYear;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-
-}
